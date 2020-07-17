b3c2e7e3de2d52e851380aba10a6373da8362db4
sparsell <hello@sparsell.com>
2020-07-17 10:07:37 -0400

    edits to UI

--- lib/game.rb
+++ lib/game.rb
@@ -13,30 +13,45 @@ class Game
   [6, 4, 2]
 ]
 
-  def initialize (player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
     @board = board
     @player_1 = player_1
     @player_2 = player_2
   end
 
-  # def start
-  #   puts "Hello player!"
-  #
-  #   puts "What kind of game do you want to play?"
-  #   puts "You can: "
-  #   puts "- watch the computer play"
-  #   puts "- play against the computer"
-  #   puts "or simply play with someone else"
-  #   puts "Which player should play 'X' first?"
-  #
-  #
-  #   puts "Would you like to play again?"
-  #
-  #
-  #
-  #   puts "Good-bye!"
-  #
-  # end
+  def start
+  
+    puts "What kind of tictactoe game do you want to play?"
+    puts ""
+    puts "You can: "
+    puts ""
+    puts "1. - play with someone else"
+    puts "2. - play against the computer"
+    puts "3. - watch the computer play"
+    puts "4. - exit the game"
+    puts ""
+    puts "Please enter a number from 1 - 4"
+      input = gets.to_i
+
+      if input == 1
+        #two human players
+        puts Board::new
+        puts "The first player to move is 'X'"
+        puts "Enter your first move, 1-9:"
+        play
+      elsif input == 2
+        #one human player against computer
+      elsif input == 3
+        #watch computer play
+      else
+        puts "Goodbye!"
+      end
+
+    # puts "Which player should play 'X' first?"
+    
+    # puts "Would you like to play again?"
+
+  end
 
   def current_player
     @board.turn_count % 2 == 0 ? player_1 : player_2
@@ -91,9 +106,13 @@ end
       end
         if draw?
           puts "Cat's Game!"
+          puts ""
         elsif won?
           puts "Congratulations #{winner}!"
         end
+        puts ""
+        puts "Do you want to play again?"
+        start
   end
 
 end
